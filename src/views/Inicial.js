2f04309003eb45483833844bde097b042b41b009
SessyoinKirin <neosdiva@gmail.com>
2023-10-22 15:54:29 -0300

    instrução para usuário mobile não perder a experiência

--- src/views/Inicial.js
+++ src/views/Inicial.js
@@ -3,6 +3,13 @@ import { Outlet } from 'react-router-dom'
 import '../css/inicial.css'
 
 export default function Inicial() {
+  function forceLandscape() {
+    if (window.orientation === 0 || window.orientation === 180) {
+      alert('Para melhorar sua experiencia, por favor, gire seu dispositivo para o modo paisagem.');
+    }
+  }
+  window.addEventListener('orientationchange', forceLandscape);
+  forceLandscape();
   return (
     <div className='wallpaper'>
       <Outlet></Outlet>
